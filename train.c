d722cf615c266ccd89ffe6fb1770dbcf564e795a
icecreamsoldier <mariana.9000@hotmail.com>
2018-10-04 12:45:00 -0500

    Still no

--- train.c
+++ train.c
@@ -1,70 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
 #include <time.h>
-#include <pwd.h>
-#include <grp.h>
-#include <string.h>
-#include <errno.h>
-#include <ctype.h>
 #include <math.h>
 
-int population = 40000;
-double lambda[3] = {6000, 10500, 8100}, miuT = 3600;
-double timePassed = 0;
+#define CUSTOMERS 30000
+#define MAX_CAPACITY 1200
 
-int main(int argc, char* argv[]) {
-  srand(time(0));
-  int i = 0, i_break;
-  int currentLambda = 0;
-  double aTime = 0;
-  double ssTime = 0;
-  double sstart = 0, sends = 0, w=0, wq, w1=0, w2=0, w3=0, this_time;
+int main(){
+  srand(time(NULL));
+  double arriving_time[CUSTOMERS] = {0};
+  double arriving_turnstiles[CUSTOMERS] = {0};
+  double leaving_turnstiles[CUSTOMERS] = {0};
+  double waiting_time[CUSTOMERS] = {0};
+  double turnstiles_time;
+  int lambda = 100, lambda_turnstiles = 60, train_departure = 10, i, flag = 1;
+  int  total_waiting = 0, waiting_people = 0, people_queue_train=0, people_train;
 
-  for(i = 0; i < population; i++) {
-    if(timePassed >= 60 && currentLambda < 2) {
-      currentLambda++;
+  arriving_turnstiles[0] = arriving_time[0] = (-1.0/lambda) * log(rand() * 1.0 / RAND_MAX);
+  leaving_turnstiles[0] = arriving_turnstiles[0] + (-1.0 / lambda_turnstiles) * log(rand() * 1.0 / RAND_MAX);
+
+
+  for (i = 1; i < CUSTOMERS; i++) {
+    if(arriving_time[i - 1] > 60) {
+        lambda = (arriving_time[i - 1] < 120) ? 175 : 135;
+        lambda_turnstiles = 140;
     }
-    if(timePassed > 2*60){
-      miuT = 4800;
-    }
-    // printf("timePassed = %lf, Wq = %lf\n", timePassed, wq);
-    if(i != 0) {
-      aTime = aTime - ((1/lambda[currentLambda]) * log((rand() + 1.0) / (RAND_MAX+2.0)));
-      if(sends < aTime){
-        sstart = aTime;
-      }else{
-        sstart = sends;
-      }
-    }
-    ssTime = (1/miuT) * log(rand());
-    sends= ssTime + sstart;
-    // printf("aTime: %lf, ssTime: %lf, sends: %lf, sstart: %lf, Lambda: %.1lf\n", aTime, ssTime, sends, sstart, lambda[currentLambda]);
-    w = sends - aTime;
-    wq = sstart - aTime;
-    timePassed += w;
-    if(currentLambda == 0){
-      i_break = i + 1;
-      w1 = timePassed / i_break;
-      this_time = timePassed;
-    }
-    if (currentLambda == 1){
-      w2 = (timePassed - this_time) / (i + 1 - i_break);
-      this_time = timePassed;
-      i_break = i + 1;
-    }
-    if (currentLambda == 2){
-      w3 = (timePassed - this_time) / (i + 1 - i_break);
-      this_time = timePassed;
-    }
-    printf("timePassed %lf, this_time %lf, i %i, i_break %i\n", timePassed, this_time, i, i_break);
-    // printf("W = %lf, Wq = %lf\n", w, wq);
+
+    arriving_time[i] = (-1.0/lambda) * log(rand() * 1.0 / RAND_MAX)+arriving_time[i-1];
+    arriving_turnstiles[i] = ( arriving_time[i] > leaving_turnstiles[i - 1]) ? arriving_time[i] : leaving_turnstiles[i - 1];
+    turnstiles_time = (-1.0 / lambda_turnstiles) * log(rand() * 1.0 / RAND_MAX);
+    leaving_turnstiles[i] = arriving_turnstiles[i] + turnstiles_time;
   }
-  printf("w1 %lf, w2 %lf, w3 %lf\n", w1, w2, w3);
-  return 0;
+
+
+  for (i = 0; i < CUSTOMERS; i++){
+
+    if(leaving_turnstiles[i] > train_departure || people_queue_train >= 1200){
+        people_train = (people_queue_train < 1200)? people_queue_train : 1200;
+        people_queue_train = (people_queue_train - MAX_CAPACITY>0)? people_queue_train - MAX_CAPACITY : 0;
+        train_departure += 10;
+      }
+
+    if( (leaving_turnstiles[i] >= 60 && (flag & 0b1)) || (leaving_turnstiles[i] >= 120 && (flag & 0b10))){
+        printf("Waiting time between  %i pm and %i pm %f\n\n", flag+3, flag+4, total_waiting * 1.0 / waiting_people);
+        flag = flag << 1;
+        total_waiting = 0;
+        waiting_people = 0;
+    }
+
+    waiting_time[i] = train_departure - arriving_time[i];
+    total_waiting += waiting_time[i];
+    waiting_people ++;
+    people_queue_train ++;
+  }
+
+
+  if (people_queue_train > 0) {
+    people_train = (people_queue_train < 1200)? people_queue_train : 1200;
+    people_queue_train = (people_queue_train - MAX_CAPACITY>0) ? people_queue_train - MAX_CAPACITY : 0;
+    train_departure += 10;
+  }
+
+  printf("Waiting time after 7 pm: %f \n\n", total_waiting * 1.0 / waiting_people);
 }
